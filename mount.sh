975bb3ddb93ae08a89d92ae7d0e3bd9ecf8a788e
autowebsite <cclerc@cclerc.live>
2023-09-28 11:13:21 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://tracker.physlets.org/installers/download.php?file=Tracker-6.1.3-osx-installer.dmg"
+dmg_url="https://github.com/TuringSoftware/CrystalFetch/releases/download/v2.1.0/CrystalFetch.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
