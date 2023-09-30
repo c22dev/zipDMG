9a23c8087245e25451ab6bc78a550aa350ceec4c
autowebsite <cclerc@cclerc.live>
2023-09-30 16:31:07 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://bitbucket.org/objective-see/deploy/downloads/ReiKey_1.4.2.zip"
+dmg_url="https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncher.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
