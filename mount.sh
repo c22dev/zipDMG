c2a3aa4788db3a13b704219aacb96c1db9c59b5a
autowebsite <cclerc@cclerc.live>
2023-10-01 20:02:51 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="EpicInstaller-15.7.0.dmg"
+dmg_url="https://store.epicgames.com/ https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Mac/EpicInstaller-15.7.0.dmg?launcherfilename=EpicInstaller-15.7.0.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
