463c8fe8943715db4139ae3fcb86eeaf86088920
autowebsite <cclerc@cclerc.live>
2023-10-01 22:25:20 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://download.nvidia.com/gfnpc/GeForceNOW-release.dmg"
+dmg_url="https://store.steampowered.com/ https://cdn.cloudflare.steamstatic.com/client/installer/steam.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
