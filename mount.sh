d897472a33e73fe7394cdc81472c626a37ba7d2b
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-04 11:46:17 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.elmedia-video-player.com/fr/"
+dmg_url="https://cdn.akamai.steamstatic.com/client/installer/steam.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
