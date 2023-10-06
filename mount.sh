2c64ea884d511ae0203f985c4345102e466f69a7
autowebsite <cclerc@cclerc.live>
2023-10-07 01:07:39 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://dl.dolphin-emu.org/builds/cf/ce/dolphin-master-5.0-19870-universal.dmg"
+dmg_url="https://github.com/PlayCover/PlayCover/releases/download/3.0.0-beta.1/PlayCover_3.0.0-beta.1.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
