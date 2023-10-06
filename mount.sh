e516a0cd7ec89d33b99830408924ca302d29dfb6
autowebsite <cclerc@cclerc.live>
2023-10-06 11:58:40 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://download.sqlitebrowser.org/DB.Browser.for.SQLite-arm64-3.12.2.dmg"
+dmg_url="https://devimages-cdn.apple.com/design/resources/download/SF-Symbols-5.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
