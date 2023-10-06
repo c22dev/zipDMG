1b0dca904cf2bac7125b748ba796ff791ef1617b
autowebsite <cclerc@cclerc.live>
2023-10-06 09:12:20 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.dropbox.com/scl/fi/fz5kv913c7xe03ian3kkt/WiFi-Explorer-Pro-3-3.5.3-TNT.dmg?rlkey=j8g3exnbu4r92yhdlti2rkxkc&dl=1"
+dmg_url="https://releases.arc.net/release/Arc-latest.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
