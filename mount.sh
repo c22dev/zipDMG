e798c7346b3fd35a3d4b911cb4a9c704dd6f97b4
autowebsite <cclerc@cclerc.live>
2023-10-06 23:28:31 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://d241.userscloud.net/d/hul5tqustn2fvxijvhmzfecaknvnllquga2egd4blrw6snraqu3fzwm4sfxjdk2yndsdm4q5/Geometry_dash_2_113.dmg"
+dmg_url="https://www.opera.com/fr/computer/thanks?ni=stable&os=mac&utm_source=bing&utm_medium=pa&utm_campaign=Worldwide+-+Brand+-+EN&utm_content=Branded+-+Exact"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
