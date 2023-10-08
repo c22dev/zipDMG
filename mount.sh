ffe505a0eb87ab90cc06daeb4891b9aae92266cc
autowebsite <cclerc@cclerc.live>
2023-10-08 11:33:51 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://download.adguard.com/d/24276/AdGuardInstaller.dmg?source=page_fr_welcome&exid=5b307mtsercw40cw8sso4soos"
+dmg_url="https://github.com/cemu-project/Cemu/releases/download/v2.0-55/cemu-2.0-55-macos-12-x64.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
