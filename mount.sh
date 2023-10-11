8fca82cab368357f060805d6773e92dc823a787f
autowebsite <cclerc@cclerc.live>
2023-10-11 22:51:13 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.google.com/chrome/next-steps.html?statcb=0&installdataindex=empty&defaultbrowser=0"
+dmg_url="https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
