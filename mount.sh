4b83a5cc774334961f6f35778e0242a135f34d78
autowebsite <cclerc@cclerc.live>
2023-10-11 22:56:45 +0200

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://www.dropbox.com/scl/fi/qu0p4d62sl8fp3bhc6v0v/Unite-4.6-TNT.dmg?rlkey=51zuw9216fjcqqr5vvm5el2bk&dl=0"
+dmg_url="https://www.dropbox.com/scl/fi/qu0p4d62sl8fp3bhc6v0v/Unite-4.6-TNT.dmg?rlkey=51zuw9216fjcqqr5vvm5el2bk&dl=1"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
