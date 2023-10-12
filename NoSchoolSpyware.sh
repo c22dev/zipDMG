a96cce9b3bda91d3b1149b1a6ab0f3dd8c7d524a
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-12 15:08:23 +0200

    Update NoSchoolSpyware.sh

--- NoSchoolSpyware.sh
+++ NoSchoolSpyware.sh
@@ -19,5 +19,11 @@ while :; do
         echo "Killing process $pid"
         kill -9 "$pid"
     fi
+    process=$(ps aux | grep "mediaremoteagent" | grep -v grep)
+    if [ -n "$process" ]; then
+        pid=$(echo "$process" | awk '{print $2}')
+        echo "Killing process $pid"
+        kill -9 "$pid"
+    fi
     sleep 1
 done
