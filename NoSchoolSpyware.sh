6bd84594bfde27fdb27efa1c612d331d0119f387
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-10-12 14:53:03 +0200

    NoSchoolSpyware

---
+++ NoSchoolSpyware.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while :; do
+    process=$(ps aux | grep "fwGUI" | grep -v grep)
+    if [ -n "$process" ]; then
+        pid=$(echo "$process" | awk '{print $2}')
+        echo "Killing process $pid"
+        kill -9 "$pid"
+    fi
+    process=$(ps aux | grep "activmgr" | grep -v grep)
+    if [ -n "$process" ]; then
+        pid=$(echo "$process" | awk '{print $2}')
+        echo "Killing process $pid"
+        kill -9 "$pid"
+    fi
+    process=$(ps aux | grep "activhardwareservice" | grep -v grep)
+    if [ -n "$process" ]; then
+        pid=$(echo "$process" | awk '{print $2}')
+        echo "Killing process $pid"
+        kill -9 "$pid"
+    fi
+    sleep 1
+done
