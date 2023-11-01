3174666d97c3d2a7e9e5a4d8eb8c69fb105672f1
autowebsite <cclerc@cclerc.live>
2023-11-01 23:53:28 +0100

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://github.com/minecraft-linux/macos-builder/releases/download/v0.1-beta-24-311/Minecraft_Bedrock_Launcher_v0.1-beta-24-macOS-x86_64-0.2.311_macOS_10.13.0.dmg"
+dmg_url="https://sideloadly.io/SideloadlySetup.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
