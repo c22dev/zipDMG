20b0803152e9212d144fc9e8b62c2ce4a6e7e767
autowebsite <cclerc@cclerc.live>
2023-11-07 12:05:53 +0100

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://sideloadly.io/SideloadlySetup.dmg"
+dmg_url="https://dl.devmate.com/com.macpaw.site.theunarchiver/TheUnarchiver.dmg"
 
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
