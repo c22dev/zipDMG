a3e7538dd574920e624352ede324e07a29e0dd4a
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-16 15:48:06 +0100

    Apple Archive Migration

--- mount.sh
+++ mount.sh
@@ -2,12 +2,11 @@
 
 # Set the URL of the DMG file to download. This is only sample URL
 dmg_url="https://github.com/rxhanson/Rectangle/releases/download/v0.74/Rectangle0.74.dmg"
-
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
-# Set the name of the output ZIP file
-output_zip="output.tar.gz"
+# Set the name of the output AAR file
+output_aar="output.aar"
 
 # Download the DMG file
 echo "Downloading DMG file..."
@@ -23,8 +22,8 @@ if [ $? -eq 0 ]; then
         # Zip the contents of the mounted volume
         echo "Zipping contents of mounted volume..."
         cd "/Volumes/DMG_Mount" || exit
-        tar -czf "" . > /dev/null
-        tar -czf /tmp/$output_zip . > /dev/null
+        rm -f Applications
+        aa archive -d . -o /tmp/$output_aar
 
         # Unmount the DMG file
         echo "Unmounting DMG file..."
