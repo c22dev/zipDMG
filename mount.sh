01307a5b5dedfc52b12e3d2f0471645346e35d25
autowebsite <cclerc@cclerc.live>
2023-12-21 01:42:55 +0100

    web commit

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://raw.githubusercontent.com/c22dev/zipDMG/main/.github/workflows/blank.yml"
+dmg_url="https://zipdmg.c22code.repl.co/"
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
