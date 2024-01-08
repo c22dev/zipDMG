a01a34ad54022cc5304cab4df13de28060ef2975
c22dev <102235607+c22dev@users.noreply.github.com>
2024-01-08 08:30:57 +0100

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url=""
+dmg_url="https://d9458.userscloud.net/d/amlyca7ltn2fvxijzlnz7fs5lrgw4byxlyw2wklqtnzul4msuykj7lwukfrbq67qrhbwqiiu/Geometry_Dash_v2_2_MacOS.dmg"
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
