4248b17f7f06ab7cbfa36f4dfae15570b71903d6
c22dev <102235607+c22dev@users.noreply.github.com>
2024-01-09 12:34:08 +0100

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL of the DMG file to download. This is only sample URL
-dmg_url="https://ftp.osuosl.org/pub/musescore-nightlies/macos/3x/stable/MuseScore-3.6.2.548020600.dmg"
+dmg_url="https://www.hopperapp.com/downloader/hopperv4/Hopper-5.13.5-demo.dmg"
 # Set the output directory where the DMG will be mounted
 mount_dir="/Volumes/DMG_Mount"
 
